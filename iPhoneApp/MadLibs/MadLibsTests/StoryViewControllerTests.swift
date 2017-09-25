4dab2092377ac834deae4672bf3ae84613e2710c
Ginny <ginny.pennekamp@gmail.com>
2017-09-25 12:00:47 -0700

    TESTS: Unit Tests for StoryViewController

--- iPhoneApp/MadLibs/MadLibsTests/StoryViewControllerTests.swift
+++ iPhoneApp/MadLibs/MadLibsTests/StoryViewControllerTests.swift
@@ -21,6 +21,10 @@ class StoryViewControllerTests: XCTestCase {
             .instantiateViewController(
                 withIdentifier: "storyView")
             as! StoryViewController
+        
+        sut.story = Story()
+        sut.story.answers = ["one", "two", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+        
         _ = sut.view
     }
     
@@ -32,7 +36,6 @@ class StoryViewControllerTests: XCTestCase {
     
     // TODO: Test with property injection
     
-    /*
     func test_HasStoryTextLabel() {
         XCTAssertNotNil(sut.storyTextLabel)
     }
@@ -54,5 +57,62 @@ class StoryViewControllerTests: XCTestCase {
     func test_storyPage_IsInitiallyZero() {
         XCTAssertEqual(sut.storyPage, 0)
     }
-    */
+    
+    // MARK: - Test Initial Display
+    
+    func test_generateStory_GeneratesStory() {
+        let storyTitle = sut.story.story[0]
+        let expectedTitle = "ONE AND THE THREE TWOS"
+        XCTAssertEqual(storyTitle, expectedTitle)
+    }
+    
+    func test_displayStoryTitle_DisplaysStoryTitle() {
+        let labelText = sut.storyTextLabel.text
+        let expectedTitle = "ONE AND THE THREE TWOS"
+        XCTAssertEqual(labelText, expectedTitle)
+        let buttonText = sut.tellStoryButton.title(for: .normal)
+        let expectedButtonText = "Tell My Story"
+        XCTAssertEqual(buttonText, expectedButtonText)
+    }
+    
+    // MARK: - Test Storytelling
+    
+    func test_tellStory_turnsPage() {
+        let initialPageNumber = sut.storyPage
+        XCTAssertEqual(initialPageNumber, 0)
+        sut.tellStory(sut.tellStoryButton)
+        let pageNumber = sut.storyPage
+        XCTAssertEqual(pageNumber, 1)
+    }
+    
+    func test_tellStory_displaysPageTurn() {
+        sut.storyPage = 0
+        sut.tellStory(sut.tellStoryButton)
+        let labelText = sut.story.story[1]
+        let expectedText = "  Once upon a time, one went for a walk in the 3.  Pretty soon, one came upon a house.  one knocked, and when no one answered, one walked right in."
+        XCTAssertEqual(labelText, expectedText)
+        let buttonText = sut.tellStoryButton.title(for: .normal)
+        let expectedButtonText = "Tell Me More"
+        XCTAssertEqual(buttonText, expectedButtonText)
+    }
+    
+    func test_tellStory_DisplaysEnd() {
+        sut.storyPage = sut.story.story.count - 2
+        sut.tellStory(sut.tellStoryButton)
+        let labelText = sut.storyTextLabel.text
+        let expectedText = "THE END"
+        XCTAssertEqual(labelText, expectedText)
+        let buttonText = sut.tellStoryButton.title(for: .normal)
+        let expectedButtonText = "Play Again"
+        XCTAssertEqual(buttonText, expectedButtonText)
+    }
+    
+    func test_tellStory_ResetsStory() {
+        sut.storyPage = sut.story.story.count - 1
+        sut.tellStory(sut.tellStoryButton)
+        XCTAssertEqual(sut.storyPage, 0)
+        XCTAssertEqual(sut.story.timesViewed, 0)
+        XCTAssertEqual(sut.story.answers.count, 0)
+        XCTAssertEqual(sut.story.story.count, 0)
+    }
 }
